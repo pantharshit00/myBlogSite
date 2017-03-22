49be17cbf79e62378a4eeb0c34ae8b61fcc6a4b3
pantharshit00 <pantharshit00@gmail.com>
2017-03-22 10:38:10 +0530

    Fixed bug

--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERAME,process.env.DB_PASSWORD,{
+const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST
 });
